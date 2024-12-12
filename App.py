f280e963c2f62dd99085eb5bd75ff0fe7f929c70
Lahiru Dilshan <lahirud608@gmail.com>
2024-12-12 23:49:02 +0530

    Develop Book Management

--- App.py
+++ App.py
@@ -2,67 +2,39 @@ from Models.BookManagement import BookManagement, PrintedBook, Ebook
 from Models.AuthorManagement import HandleAuthor, HandleLicense
 from Models.MemberManagement import Member, Employee
 
-# class Library:
-#     def __init__(self, library_id, library_name, address):
-#         self.library_id = library_id
-#         self.library_name = library_name
-#         self.address = address
-#         self.books = []
-#         
-
-
-
-#     def add_book(self, book):
-#         # Check for duplicate book
-#         for existing_book in self.books:
-#             if existing_book.book_id == book.book_id:
-#                 print(f"Book with ID {book.book_id} already exists")
-#                 return
-
-#         # Add the book
-#         self.books.append(book)
-#         print(f"Book '{book.title}' (ID: {book.book_id}) has been successfully")
-
-#     def remove_book(self, book_id):
-#         for book in self.books:
-#             if book.book_id == book_id:
-#                 self.books.remove(book)
-#                 print(f"Book with ID {book_id} has been removed")
-#                 return
-#         print(f"Book with ID {book_id} not found in the library.")
-
 if __name__ == "__main__":
-    bk = BookManagement()
     pbk = PrintedBook()
     ebk = Ebook()
     ha = HandleAuthor()
     hl = HandleLicense()
 
-    # bk.add_book("Guitar lesson 1", "111111", "Mark", "Music", "Available")
-    # bk.add_book("Python Basics", "222222", "John Doe", "Programming", "Available")
-    # bk.add_book("Cooking 101", "333333", "Jane Smith", "Cooking", "Unavailable")
-    # bk.add_book("Astronomy Guide", "444444", "Dr. Star", "Science", "Available")
-    # bk.add_book("History of Art", "555555", "Leonardo", "Art", "Available")
-    # bk.get_book_details()
+    # pbk.add_book("Guitar lesson 1", "111111", "Mark", "Music", "Available")
+    # pbk.add_book("Python Basics", "222222", "John Doe", "Programming", "Available")
+    # pbk.add_book("Cooking 101", "333333", "Jane Smith", "Cooking", "Unavailable")
+    # pbk.add_book("Astronomy Guide", "444444", "Dr. Star", "Science", "Available")
+    # pbk.add_book("History of Art", "555555", "Leonardo", "Art", "Available")
+    # pbk.get_book_details()
 
-    # bk.delete_book("Guitar lesson 1", "111111")
-    # bk.get_book_details()
+    # pbk.delete_book("Guitar lesson 1", "111111")
+    # pbk.get_book_details()
 
-    # bk.update_book_details("333333", "Music Production", "Mark", "Music", "Available")
-    # bk.get_book_details()
+    # pbk.update_book_details("333333", "Music Production", "Mark", "Music", "Available")
+    # pbk.get_book_details()
 
-    # pbk.check_status("Guitar lesson 1")
+    # pbk.check_status("Cooking 101")
 
     # pbk.update_status("Guitar lesson 1", "Unavailable")
+    # pbk.get_book_details()
 
-    # pbk.find_book("101")
+
+    # pbk.find_book("555555")
 
     # ebk.add_ebook("456123789", "Machine Learning Basics", "Alice Johnson", "Technology", "https://example.com/ml_basics", "6MB")
     # ebk.add_ebook("123456789", "Python Basics", "John Doe", "Programming", "http://example.com", "20MB")
     # ebk.add_ebook("987654321", "Guitar Lesson", "Jane Doe", "Music", "http://example.com", "5MB")
     # ebk.get_ebook_details()
     # ebk.download("456123789")
-    # ebk.get_file_size("987654321")
+    # ebk.get_file_size("123456789")
 
     # ha.add_author("A001", "Maark Anderson", "Main Street,High level.", "G123456789", "1 - Year")
     # ha.add_author("A002", "Henry Cavill", "School Road,Romania.", "G234567891", "1 1/2 - Year")
@@ -82,19 +54,19 @@ if __name__ == "__main__":
     # hl.add_license("L0005", "2 - Years")
     # hl.get_license()
 
-    print("\n")
-    member = Member()
+    # print("\n")
+    # member = Member()
 
-    member = Member("201", "Alice", "alice@example.com", 25, "Standard", "Active")
-    member.register_member(member)
+    # member = Member("201", "Alice", "alice@example.com", 25, "Standard", "Active")
+    # member.register_member(member)
 
-    stm = Employee
+    # stm = Employee
 
-    stm.update_member_status(Employee, "M001", "Suspended")
+    # stm.update_member_status(Employee, "M001", "Suspended")
     
 
-    print("\n=== Member Operations ===")
-    member.update_contact_info("alice_new@example.com")
-    book_4 = BookManagement("101", "Python Programming", "123456789", "John Doe", "Programming")
-    member.borrow_book(book_4)
-    member.return_book(book_4)
+    # print("\n=== Member Operations ===")
+    # member.update_contact_info("alice_new@example.com")
+    # book_4 = BookManagement("101", "Python Programming", "123456789", "John Doe", "Programming")
+    # member.borrow_book(book_4)
+    # member.return_book(book_4)
--- Models/BookManagement.py
+++ Models/BookManagement.py
@@ -7,98 +7,104 @@ class BookManagement:
         self.status = status
         self.link = link
         self.file_size = file_size
-        self.books = {}
+        self.books = []
 
     def get_book_details(self):
+        print("\n========== All Book Details ==========")
         if not self.books:
             print("Library is Empty.")
         else:
-            print("\n========== All Book Details ==========")
-            for index, (isbn, details) in enumerate(self.books.items(), start=1):
-                title = details.get('Title', 'N/A')
-                author = details.get('Author', 'N/A')
-                isbn = details.get('ISBN', 'N/A')
-                category = details.get('Category', 'Unknown')
-                status = details.get('Status', "0MB")
-                print(f"\nTitle: {title}\nISBN: {isbn}\nAuthor: {author}\nCategory: {category}\nStatus: {status}")
+            for book in self.books:
+                print(f"Title: {book['Title']}\nISBN: {book['ISBN']}\nAuthor: {book['Author']}\nCategory: {book['Category']}\nStatus: {book['Status']}\n")
 
     def add_book(self, title, isbn, author, category, status):
         print("\n========== Add Book ==========")
         if isbn in self.books:
-            print(f"Book with ISBN '{isbn}' already exists!")
+                print(f"Book with ISBN: {book.isbn} already exists")
+                return
         else:
-            self.books[isbn] = {
+            book = {
                 "Title": title,
                 "ISBN": isbn,
                 "Author": author,
                 "Category": category,
                 "Status": status
             }
+            self.books.append(book)
             print(f"Book '{title}' Added Successfully!")
             return
         
     def delete_book(self, title, isbn):
         print("\n========== Delete The Book ==========")
-        if isbn in self.books:
-            print(f"ISBN: {isbn}\nTitle: {title}\nSuccessfully Deleted")
-            del self.books[isbn]
-        else:
-            print(f"Book is Not Found!")
-
-    def update_book_details(self, isbn, title, author, category, status):
-        print("\n========== Update Book ==========")
-        if isbn not in self.books:
-            print(f"Book is Not Found")
-            return
-        book = self.books[isbn]
-        if title:
-            book["Title"] = title
-        if author:
-            book["Author"] = author
-        if category:
-            book["Category"] = category
-        if status:
-            book["Status"] = status
-        print(f"ISBN: {isbn}\nTitle: {title}\nAuthor: {author}\nCategory: {category}\nStatus: {status}\nUpdated Successfully!")
-
-    def find_book(self, book_id):
         for book in self.books:
-            if book.book_id == book_id:
-                print(f"Found Book:\n{book.get_book_details()}")
-                return book
-        print(f"Book with ID {book_id} not found in the library.")
-        return None
+            if title == book['Title']:
+                self.books.remove(book)
+                print(f"ISBN: {isbn}\nTitle: {title}\nSuccessfully Deleted")
+                return
+        print(f"Book is Not Found!")
+
+    def update_book_details(self, isbn, new_title, new_author, new_category, new_status):
+        print("\n========== Update Book ==========")
+        for book in self.books:
+            if book['ISBN'].lower() == isbn.lower():
+                if new_title:
+                    book['Title'] = new_title
+                if new_author:
+                    book['Author'] = new_author
+                if new_category:
+                    book['Category'] = new_category
+                if new_status:
+                    book['Status'] = new_status
+                print(f"Book '{isbn}' updated successfully!")
+                return
+        print(f"Book with title '{isbn}' not found.")
+
+    def find_book(self, isbn):
+        print("\n========== Find the Book ==========")
+        for book in self.books:
+            if isbn == book['ISBN']:
+                print(f"ISBN: {isbn}\nTitle: {book['Title']}\nAuthor: {book['Author']}\nCategory: {book['Category']}\nStatus: {book['Status']}\n>>>>> Book is Found")
+                return
+        print("Book is Not Found")
 
 class PrintedBook(BookManagement):
+    def __init__(self, title=None, isbn=None, author_name=None, category='Unknown', status='Available', link='', file_size=''):
+        super().__init__(title, isbn, author_name, category, status, link, file_size)
+
     def check_status(self, title):
         print("\n========== Check Book Status ==========")
-        print(f"Title: {title}\nStatus: {'Available' if self.status else 'Unavailable'}\n")
+        for book in self.books:
+            if book['Title'] == title:
+                print(f"Title: {book['Title']}\nStatus: {book['Status']}")
+                return
+        print(f"Book '{title}' not found.\n")
 
     def update_status(self, title, next_status):
-        print("========== Update Book Status ==========")
-        prev_status = self.status
-        if prev_status != next_status:
-            self.status = next_status
-            print (f"Title: {title}\nStatus Updated: {prev_status} to {self.status} successfully.")
-            return
-        else:
-            print(f"{title} is already {prev_status}.")
-            return
-PrintedBook()
+        print("\n========== Update Book Status ==========")
+        for book in self.books:
+            if book['Title'] == title:
+                prev_status = book['Status']
+                if prev_status != next_status:
+                    book['Status'] = next_status
+                    print(f"Title: {title}\nStatus Updated: {next_status}\n>>>>> Status Updated Successfully")
+                else:
+                    print(f"Title: {book['Title']}\nStatus: {next_status}\n>>>>> Already Existed")
+                return
+        print(f"Book is Not Found.")
         
 class Ebook(BookManagement):
     def __init__(self, title=None, isbn=None, author_name=None, category='Unknown', status='Available', link='', file_size=''):
         super().__init__(title, isbn, author_name, category, status, link, file_size)
         self.link = link
         self.file_size = file_size
-        self.books = {}
+        self.books = []
 
     def add_ebook(self, isbn, title, author, category, link, file_size):
         print("\n========== Add E-Book ==========")
         if isbn in self.books:
-            print(f"Book with ISBN '{isbn}' already exists!")
+            print(f"Book with ISBN {ebook.isbn} Already Exists!")
         else:
-            self.books[isbn] = {
+            ebook = {
                 "Title": title,
                 "ISBN": isbn,
                 "Author": author,
@@ -106,34 +112,27 @@ class Ebook(BookManagement):
                 "Link": link,
                 "Size": file_size
             }
-            print(f"Title: {title}\nISBN: {isbn}\nAuthor: {author}\nCategory: {category}\nLink: {link}\nSize: {file_size}\nAdded Successfully!")
+            self.books.append(ebook)
+            print(f"Book '{title}' Added Successfully!")
 
     def get_ebook_details(self):
         if not self.books:
             print("Library is Empty.")
         else:
             print("\n========== All E-Book Details ==========")
-            for index, (isbn, details) in enumerate(self.books.items(), start=1):
-                title = details.get('Title', 'N/A')
-                author = details.get('Author', 'N/A')
-                isbn = details.get('ISBN', 'N/A')
-                category = details.get('Category', 'Unknown')
-                link = details.get('Link', 'N/A')
-                size = details.get('Size', "0MB")
-                print(f"\nTitle: {title}\nISBN: {isbn}\nAuthor: {author}\nCategory: {category}\nLink: {link}\n(Size: {size})")
+            for ebook in self.books:
+                print(f"Title: {ebook['Title']}\nISBN: {ebook['ISBN']}\nAuthor: {ebook['Author']}\nCategory: {ebook['Category']}\nLink: {ebook['Link']}\nSize: {ebook['Size']}\n")
 
     def download(self, isbn):
         print("\n========== E-Book Download Link ==========")
-        if isbn in self.books:
-            print(f"Title: {self.books[isbn]['Title']}\nLink: {self.books[isbn]['Link']}")
-        else:
-            print(f"E-Book with ISBN {isbn} is Not Found")
+        for ebook in self.books:
+            if isbn in ebook['ISBN']:
+                print(f"Title: {ebook['Title']}\nLink: {ebook['Link']}")
+        return f"E-Book is Not Found"
             
-
     def get_file_size(self, isbn):
-        """Get the file size of the e-book."""
         print("\n========== E-Book Size ==========")
-        if isbn in self.books:
-            print(f"Title: {self.books[isbn]['Title']}\nFile Size: {self.books[isbn]['Size']}")
-        else:
-            print(f"E-Book with ISBN {isbn} is Not Found")
+        for ebook in self.books:
+            if isbn in ebook['ISBN']:
+                print(f"Title: {ebook['Title']}\nFile Size: {ebook['Size']}")
+        return f"E-Book with ISBN {isbn} is Not Found"
