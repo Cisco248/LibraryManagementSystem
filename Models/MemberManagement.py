fdf021eb8e7019759b1446633063e954faf55812
Lahiru Dilshan <lahirud608@gmail.com>
2024-12-13 01:59:19 +0530

    Develop the Member Management

--- Models/MemberManagement.py
+++ Models/MemberManagement.py
@@ -1,43 +1,68 @@
 class Member:
-    def __init__(self, member_id=None, member_name=None, contact_info=None, age=0, membership_type=None, membership_status=None):
+    def __init__(self, member_id=None, member_name=None, contact_no=None, age=0, membership_type=None, membership_status=None):
         self.member_id = member_id
         self.member_name = member_name
-        self.contact_info = contact_info
+        self.contact_no = contact_no
         self.age = age
         self.membership_type = membership_type
         self.membership_status = membership_status
         self.borrowed_books = []
         self.members = []
+        self.books = []
 
-    def register_member(self, member):
-        for existing_member in self.members:
-            if existing_member.member_id == member.member_id:
-                print(f"Member with ID {member.member_id} already exists")
+    def register_member(self, member_id, member_name, contact_no, age, membership_type, membership_status):
+        print("\n========== Add Member ==========")
+        if member_id in self.members:
+            print(f"Member is Existed!")
+        else:
+            member = {
+                "Member ID": member_id,
+                "Member Name": member_name,
+                "Contact No": contact_no,
+                "Age": age,
+                "Member Type": membership_type,
+                "Membership Status": membership_status
+            }
+            self.members.append(member)
+            print(f"Member '{member_id}' Added Successfully!")            
+            return
+
+    def update_member(self, member_id, new_member_name, new_contact_no, new_age, new_member_type, new_membership_status):
+        print("\n========== Update Member ==========")
+        for member in self.members:
+            if member['Member ID'] == member_id:
+                if new_member_name:
+                    member['Member Name'] = new_member_name
+                if new_contact_no:
+                    member['Contact No'] = new_contact_no
+                if new_age:
+                    member['Age'] = new_age
+                if new_member_type:
+                    member['Member Type'] = new_member_type
+                if new_membership_status:
+                    member['Membership Status'] = new_membership_status
+                print(f"Member ID: {member['Member ID']}\nMember Name: {member['Member Name']}\nContact No: {member['Contact No']}\nAge: {member['Age']}\nMember Type: {member['Member Type']}\nMembership Status: {member['Membership Status']}\n>>>>> Update Successfully")
                 return
-        self.members.append(member)
+            else:
+                return f"Member Not Found"
+    
+    def delete_member(self, member_id):
+        print("\n========== Delete Member ==========")
+        for member in self.members:
+            if member['Member ID'] == member_id:
+                self.members.remove(member)
+                print(f"Member ID: {member['Member ID']}\nMember Name: {member['Member Name']}\n>>>>> Successfully Deleted")
+                return
+        print(f"Member Not Found!")
 
-    def update_contact_info(self, new_contact_info):
-        if self.contact_info != new_contact_info:
-            prev_contact_info = self.contact_info
-            self.contact_info = new_contact_info
-            print(f"{self.member_name}'s previous contact info: {prev_contact_info} updated to {self.contact_info}")
+    def get_member_details(self):
+        print("\n========== All Member Details ==========")
+        if not self.members:
+            print("Member List is Empty.")
         else:
-            print(f"{self.member_name}'s contact info is the same as the new one, no update needed.")   
+            for member in self.members:
+                print(f"Member ID: {member['Member ID']}\nMember Name: {member['Member Name']}\nContact No: {member['Contact No']}\nAge: {member['Age']}\nMember Type: {member['Member Type']}\nMembership Status: {member['Membership Status']}\n")
 
-    def borrow_book(self, book):
-        if self.membership_status == "Active":
-            self.borrowed_books.append(book)
-            print(f"{self.member_name} borrowed the book: {book.title}")
-        else:
-            print(f"{self.member_name}'s membership is not active, cannot borrow books.")
-        
-    def return_book(self, book):
-        if book in self.borrowed_books:
-            self.borrowed_books.remove(book)
-            print(f"{self.member_name} returned the book: {book.title}")
-        else:
-            print(f"{self.member_name} has not borrowed the book: {book.title}.")
-        
     def upgrade_membership(self):
         if self.membership_type == "Standard":
             self.membership_type = "Premium"
@@ -52,9 +77,25 @@ class Member:
         else:
             print(f"{self.member_name}'s membership is already Standard.")
 
+    def borrow_book(self, isbn):
+        print("========== Book Barrow Details ==========")
+        for book in self.books:
+            if book['ISBN'] == isbn:
+                print(f"ISBN: {book['ISBN']}\nTitle: {book['Title']}\nBook is Barrowed")
+                return
+            else:
+                return f"Please Enter the Valid ISBN"
+        
+    def return_book(self, book):
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+            print(f"{self.member_name} returned the book: {book.title}")
+        else:
+            print(f"{self.member_name} has not borrowed the book: {book.title}.")
+
 class Employee(Member):
-    def __init__(self, member_id, member_name, contact_info, age, membership_type, membership_status, employee_id, position, salary):
-        super().__init__(member_id, member_name, contact_info, age, membership_type, membership_status)
+    def __init__(self, member_id=None, member_name=None, contact_no=None, age=0, membership_type=None, membership_status=None, employee_id=None, position=None, salary=None):
+        super().__init__(member_id, member_name, contact_no, age, membership_type, membership_status)
         self.employee_id = employee_id
         self.position = position
         self.salary = salary
